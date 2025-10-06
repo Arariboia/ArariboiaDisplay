d7c5b43cbe350e6d949426457bad31dcf7a36856
PCBMoreira0 <pcbmoreira@id.uff.br>
2025-10-06 01:44:52 -0300

    add changing style before showing window

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QScreen>
 #include <QDebug>
 #include <QStyleFactory>
+
 int main(int argc, char *argv[])
 {
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard")); // ativa o teclado virtual (é necessário para digitar em displays touch)
@@ -12,6 +13,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QApplication::setStyle(QStyleFactory::create("Fusion"));
     MainWindow w;
+    QTime time = QTime::currentTime();
+    int currentHour = time.hour();
+    if(currentHour >= 18 || currentHour < 7){
+        w.setStyleSheet(MainWindow::STYLE_DARK_MODE);
+    }else{
+        w.setStyleSheet(MainWindow::STYLE_LIGHT_MODE);
+    }
     w.show();
 
     return a.exec();
